df7fa96edfbd2dadd6d6a7c35abbcf451d918b16
ElenaGenergardt <P_L_Gen@web.de>
2025-11-09 13:36:39 +0100

    React project lesson 01

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
+function App() {
+  return (
+    <>
+      <Lesson_02 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/lessons/Lesson_02/Lesson_02.jsx
@@ -0,0 +1,9 @@
+function Lesson_02() {
+  return (
+    <div>
+      <p>Lesson_02</p>
+    </div>
+  );
+}
+
+export default Lesson_02;
